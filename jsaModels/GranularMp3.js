39f38dbea805534b895222b2ee2741c810adaf6c
Pallav Shinghal <pshinghal@gmail.com>
2012-09-24 16:21:28 +0800

    Fixed Granular synthesis. It works now, but behaves a bit strangely at lower speeds.

--- jsaModels/GranularMp3.js
+++ jsaModels/GranularMp3.js
@@ -8,81 +8,80 @@ This library is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License and GNU Lesser General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>
 ------------------------------------------------------------------------------------------*/
 function granularMp3Factory() {
-	
+
 	//Useful addition:
 	//When the file finishes playing, change release time to 0;
 	//otherwise it's confusing: Press release and it will wait for the release time, but won't really DO anything
 	//user may just click play immediately, and the architecture won't be rebuilt, so noting will happen
 
-	//CUrrently, I've just enabled looping to overcome that problem
+	//Currently, I've just enabled looping to overcome that problem
 
 	var tempNum = 0;
+	var i = 0;
 
 	var buffLoaded = false, architectureBuilt = false;
 
 	var xhr = new XMLHttpRequest();
 	var soundBuff = null;
 
-	var gainEnvNode, gainLevelNode, sourceNode;
+	var gainLevelNode;
 
 	var m_gainLevel = 0.5;
-	var m_attackTime = 0.05;
-	var m_releaseTime = 1.0;
+	// var m_attackTime = 0.05;
+	// var m_releaseTime = 1.0;
 	var m_soundUrl = "";//"./sounds/schumannLotusFlower.mp3";
-	var m_grainSize = 0.09;
+	var m_grainSize = 0.9;
 	var m_speed = 1.0;
-	var stopTime = 0.0;
-	var now = 0.0;
 
 	var bufferDuration = 1.0; //This is a very irrelevant figure at this point
 	var realTime = 0.0;
 	var grainTime = 0.0;
-	var grainDuration = 0.0;
-	var grainSpacing = 0.0;
+	var grainDuration = m_grainSize;
+	var grainSpacing = 0.25 * grainDuration;
 	var speed = m_speed;
 
+	var grainWindow;
+	var grainWindowLength = 16384;
+	grainWindow = new Float32Array(grainWindowLength);
+	for (i = 0; i < grainWindowLength; i += 1) {
+		grainWindow[i] = Math.sin(Math.PI * i / grainWindowLength);
+	}
+
 	var continuePlaying = true;
-	
+
 	var myInterface = baseSM();
 
 	function sendXhr() {
 		//SHOULD XHR BE RE-CONSTRUCTED??
 		xhr.open('GET', m_soundUrl, true);
 		xhr.responseType = 'arraybuffer';
-		xhr.onerror = function(e) {
+		xhr.onerror = function (e) {
 			console.error(e);
 		};
-		xhr.onload = function() {
+		xhr.onload = function () {
 			console.log("Sound(s) loaded");
 			soundBuff = audioContext.createBuffer(xhr.response, false);
 			buffLoaded = true;
 			bufferDuration = soundBuff.duration;
 			console.log("Buffer loaded with duration " + bufferDuration);
-			
+
 			//SHOULD THIS FUNCTION BE CALLED BEFORE CHANGING buffLoaded ???
-			buildModelArchitecture();
+			//buildModelArchitecture();
 		};
 		xhr.send();
 	}
 
 	function buildModelArchitecture() {
-		//sourceNode = audioContext.createBufferSource();
-		gainEnvNode = audioContext.createGainNode();
 		gainLevelNode = audioContext.createGainNode();
 
-		//sourceNode.buffer = soundBuff;
-		//sourceNode.loop = true;
 		// Looping is to be thought of quite differently in Granular synthesis
 		gainLevelNode.gain.value = m_gainLevel;
-		gainEnvNode.gain.value = 0;
 
-		//sourceNode.connect(gainEnvNode);
-		gainEnvNode.connect(gainLevelNode);
 		gainLevelNode.connect(audioContext.destination);
 
 		architectureBuilt = true;
 
-		console.log("architecture built");
+		//console.log("architecture built");
 	}
 
 	function scheduleGrain() {
@@ -91,66 +90,70 @@ function granularMp3Factory() {
 		//console.log("source created");
 		source.buffer = soundBuff;
 		//console.log("soundBuff created");
-		source.connect(gainEnvNode);
+
+		//TODO: See if the problem is being caused by the fact that we aren't creating NEW "gainLevelNode"s
+		buildModelArchitecture();
+
+		var grainWindowNode = audioContext.createGainNode();
+		source.connect(grainWindowNode);
+		grainWindowNode.connect(gainLevelNode);
+
 		//console.log("before noteGrainOn");
+		//console.log("scheduling noteGrainOn with " + realTime + " " + grainTime + " " + grainDuration);
 		source.noteGrainOn(realTime, grainTime, grainDuration);
 		//console.log("noteGrainOn triggered");
+		grainWindowNode.gain.value = 0.0;
+		grainWindowNode.gain.setValueCurveAtTime(grainWindow, realTime, grainDuration);
 
 		realTime += grainSpacing;
+		//console.log(realTime);
 		grainTime += grainSpacing * speed;
-
-		if (grainTime > bufferDuration) grainTime = 0.0;
-		if (grainTime < 0.0) grainTime += bufferDuration; // Not Sure why
+		//console.log("updated stuff");
+		if (grainTime > bufferDuration) {
+			grainTime = 0.0;
+		}
+		if (grainTime < 0.0) {
+			grainTime += bufferDuration; // Not Sure why
+		}
 	}
 
 	function stopScheduler() {
+		console.log("stopping");
 		continuePlaying = false;
 	}
 
 	function schedule() {
 		//console.log("schedule triggered");
-		if(!continuePlaying)
+		if (!continuePlaying) {
 			return;
+		}
 
 		var currentTime = audioContext.currentTime;
 
+		//console.log("here");
+
+		//console.log("grainTime: " + grainTime);
+		//console.log("grainSpacing: " + grainSpacing);
+		//console.log("realTime: " + realTime);
 		while (realTime < currentTime + 0.100) {
 			scheduleGrain();
+			//realTime += 0.01;
 		}
-
-		console.log(tempNum++);
+		//console.log(tempNum++);
 
 		setTimeout(schedule, 20);
 	}
 
-	myInterface.play = function(i_gain) {
+	myInterface.play = function (i_gain) {
 		if (buffLoaded) {
-			now = audioContext.currentTime;
-			// TODO: See is we can remove this whole stopTime condition
-			if (stopTime <= now) {
-				console.log("rebuilding");
-				buildModelArchitecture();
-				//sourceNode.noteOn(now);
-				console.log("after architecture build");
-				realTime = audioContext.currentTime;
-				console.log("got realTime");
-				continuePlaying = true;
-				console.log("before schedule");
-				schedule();
-				gainEnvNode.gain.value = 0;
-			} else {
-				console.log("NOT re-building");
-				gainEnvNode.gain.cancelScheduledValues(now);
-			}
-	
-			stopTime = bigNum;
-			sourceNode.noteOff(stopTime);
-			
+			realTime = audioContext.currentTime;
+			console.log("got realTime");
+			continuePlaying = true;
+			console.log("before schedule");
+			setTimeout(schedule, 0);
+
 			gainLevelNode.gain.value = i_gain || m_gainLevel;
 			console.log("Gain set at " + gainLevelNode.gain.value);
-	
-			gainEnvNode.gain.setValueAtTime(0, now);
-			gainEnvNode.gain.linearRampToValueAtTime(gainLevelNode.gain.value, now + m_attackTime);
 		} else {
 			console.log("Buffer NOT loaded yet!");
 			//CREATE EXTERNAL CALLBACK HERE!!!
@@ -166,7 +169,7 @@ function granularMp3Factory() {
 			"max": 1,
 			"val": m_gainLevel
 		},
-		function(i_val) {
+		function (i_val) {
 			gainLevelNode.gain.value = m_gainLevel = i_val;
 		}
 	);
@@ -179,7 +182,7 @@ function granularMp3Factory() {
 			"max": 1,
 			"val": m_speed
 		},
-		function(i_val) {
+		function (i_val) {
 			speed = m_speed = i_val;
 		}
 	);
@@ -192,38 +195,38 @@ function granularMp3Factory() {
 			"max": 0.5,
 			"val": m_grainSize
 		},
-		function(i_val) {
+		function (i_val) {
 			m_grainSize = i_val;
 			grainDuration = m_grainSize;
 			grainSpacing = 0.25 * grainDuration;
 		}
 	);
 
-	myInterface.setAttackTime = myInterface.registerParam(
-		"Attack Time",
-		"range",
-		{
-			"min": 0,
-			"max": 1,
-			"val": m_attackTime
-		},
-		function(i_val) {
-			m_attackTime = parseFloat(i_val);
-		}
-	);
+//  myInterface.setAttackTime = myInterface.registerParam(
+//      "Attack Time",
+//      "range",
+//      {
+//          "min": 0,
+//          "max": 1,
+//          "val": m_attackTime
+//      },
+//      function(i_val) {
+//          m_attackTime = parseFloat(i_val);
+//      }
+//  );
 
-	myInterface.setReleaseTime = myInterface.registerParam(
-		"Release Time",
-		"range",
-		{
-			"min": 0,
-			"max": 3,
-			"val": m_releaseTime
-		},
-		function(i_val) {
-			m_releaseTime = parseFloat(i_val);
-		}
-	);
+//  myInterface.setReleaseTime = myInterface.registerParam(
+//      "Release Time",
+//      "range",
+//      {
+//          "min": 0,
+//          "max": 3,
+//          "val": m_releaseTime
+//      },
+//      function(i_val) {
+//          m_releaseTime = parseFloat(i_val);
+//      }
+//  );
 
 	myInterface.setSoundUrl = myInterface.registerParam(
 		"Sound URL",
@@ -231,21 +234,18 @@ function granularMp3Factory() {
 		{
 			"val": "http://localhost/schumannLotusFlower.mp3"
 		},
-		function(i_val) {
+		function (i_val) {
 			m_soundUrl = i_val;
 			buffLoaded = false;
 			sendXhr();
 		}
 	);
 
-	myInterface.release = function() {
+	myInterface.release = function () {
 		console.log("release triggered");
-		now = audioContext.currentTime;
-		stopTime = now + m_releaseTime;
 
-		gainEnvNode.gain.linearRampToValueAtTime(0, stopTime);
-		//sourceNode.noteOff(stopTime);
-		setTimeout(stopScheduler, m_releaseTime);
+		//TODO: Remove this timeOut thing if possible
+		setTimeout(stopScheduler, 0);
 		architectureBuilt = false; //probably
 	};
 
