9367288c0755a5213f9b1c6cd606abf21b7b0787
Pallav Shinghal <pshinghal@gmail.com>
2012-10-13 20:04:31 +0800

    Fixed nrand declaration bug

--- jsaCore/utils.js
+++ jsaCore/utils.js
@@ -40,11 +40,11 @@ define(
 		utils.nrand = function (m, sd) {
 			var x1, x2, rad, y1;
 			do {
-				x1 = 2 * this.random() - 1;
-				x2 = 2 * this.random() - 1;
+				x1 = 2 * Math.random() - 1;
+				x2 = 2 * Math.random() - 1;
 				rad = x1 * x1 + x2 * x2;
 			} while (rad >= 1 || rad === 0);
-			var c = this.sqrt(-2 * Math.log(rad) / rad);
+			var c = Math.sqrt(-2 * Math.log(rad) / rad);
 			return (x1 * c) * sd + m;
 		};
 
